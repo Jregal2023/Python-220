a2a445e048bd638f16ab0a40062bd07eb75f3aee
Jregal2023 <Jregal@ivytech.edu>
2025-03-26 15:37:07 -0400

    Fixed Somed issues, and added some details

--- M02Lab(if-else, While)/main.py
+++ M02Lab(if-else, While)/main.py
@@ -1,10 +1,25 @@
 '''This is programs purpose is to enter students' enter their gpa, as well as their first and last name. 
 The program is made up of a while loop in which if you wish to cancel the program type ZZZZ
 '''
+# programmed by Jace Regal 03/26/2025 as part of SDEV 220 M02 Lab If,else While 
+
+#used to store the value of lastname, also used to ensure that the While loop can end when type ZZZ
+#First name stores the string value of the students firstname, and GPA is a float that determines which category they fit.
+
+#runs until lastname = "ZZZ"
+'''Test cases:
+lastName : Regal, firstName : Jace, GPA: 3.75
+lastName : Hamby, firstName : Zachary, GPA: 5.0
+lastName : Tharpe, firstName : Lucien, GPA: 1.4
+lastName : Quinn, firstName : Caleb, GPA: 3.3
+lastName : Hernandez, firstName : Kim, GPA: 3.6
+
+'''
 
 print("Enter the students first, and last name, as well as their GPA to see if they qualify for either Honor Roll, Deans' list, or niether")
 
 lastName = ""
+
 while True:
    
     print("Enter the students first, and last name, as well as their GPA. If you wish to quit type ZZZ in lastname ")
