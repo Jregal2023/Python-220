1e0adaf0fe2b2e6804273c41fef1d789d8bebf5f
Jregal2023 <Jregal@ivytech.edu>
2025-03-26 15:29:37 -0400

    Fixed some bugs, and issues

--- M02Lab(if-else, While)/main.py
+++ M02Lab(if-else, While)/main.py
@@ -1,19 +1,13 @@
-'''Write a Python app that will accept student names and GPAs and test if the student qualifies for either the Dean's List or the Honor Roll. Your app will:
-
-    ask for and accept a student's last name.
-    quit processing student records if the last name entered is 'ZZZ'.
-    ask for and accept a student's first name.
-    ask for and accept the student's GPA as a float.
-    test if the student's GPA is 3.5 or greater and, if so, print a message saying that the student has made the Dean's List.
-    test if the student's GPA is 3.25 or greater and, if so, print a message saying that the studnet has made the Honor Roll.
+'''This is programs purpose is to enter students' enter their gpa, as well as their first and last name. 
+The program is made up of a while loop in which if you wish to cancel the program type ZZZZ
 '''
 
-print("Enter the students first, and last name, as well as their GPA ")
+print("Enter the students first, and last name, as well as their GPA to see if they qualify for either Honor Roll, Deans' list, or niether")
 
 lastName = ""
 while True:
    
-    print("Enter the students first, and last name, as well as their GPA ")
+    print("Enter the students first, and last name, as well as their GPA. If you wish to quit type ZZZ in lastname ")
     
     lastName = input("Enter the lastname, or ZZZ to quit program ")
     if(lastName == "ZZZ"):
