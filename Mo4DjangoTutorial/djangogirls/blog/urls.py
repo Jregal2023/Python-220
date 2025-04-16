6e81f6ce14773e8ba4323e7bafa381ebc37c68dc
Jregal2023 <Jregal@ivytech.edu>
2025-04-16 14:52:43 -0400

    init

---
+++ Mo4DjangoTutorial/djangogirls/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path 
+# . means current folder
+from . import views
+
+urlpatterns = [
+    #patht that takes in 3 arguments - path url (/pagename), function
+    path('', views.post_list, name ='post_list'),
+    ]
--- Mo4DjangoTutorial/djangogirls/blog/views.py
+++ Mo4DjangoTutorial/djangogirls/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+#views is used to handle requests and provide a respons
+#we take the request info and handle it in someway
+#then we fetch the info (if needed) from the database to then send ot a HTML page
+#finally, we create a  response to send back to the client
+
+#everything in our views file takes in a request as an argument
+def post_list(request):
+    #return a response..the response will return the request back to the user
+    #tell you the template or html page to go to and any data that needs to be passed to that html page
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    
+    return render(request,'blog/post_list.html', {'Posts' : posts})
