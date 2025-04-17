06981cfb80700fa033a947bb81be8b6ca50e8220
jregal <jregal@ivytech.edu>
2025-04-16 21:12:55 -0400

    init

--- Mo4DjangoTutorial/djangogirls/mysite/settings.py
+++ Mo4DjangoTutorial/djangogirls/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'blog.urls'
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'de-ch'
+LANGUAGE_CODE = 'en-GB'
 
 TIME_ZONE = 'Europe/Berlin'
 
