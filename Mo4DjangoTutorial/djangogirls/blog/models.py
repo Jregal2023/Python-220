4b9424428e5cc45bf17e9d3b63487e3ddfd71a59
jregal <jregal@ivytech.edu>
2025-04-30 15:14:07 -0400

    init added comments section

--- Mo4DjangoTutorial/djangogirls/blog/models.py
+++ Mo4DjangoTutorial/djangogirls/blog/models.py
@@ -9,6 +9,9 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -16,3 +19,22 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete = models.CASCADE, related_name = 'comments')
+    author = models.CharField(max_length = 200)
+    text =models.TextField()
+    created_date = models.DateTimeField(default = timezone.now)
+    approved_comment = models.BooleanField(default=False)
+        
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+
+def __str__(self):
+    return self.title
+    
+    
+        
